337157fe20a234dfcf3c90beee4675ae0c12e23e
Adilson Moreira <project021126@gmail.com>
2026-08-07 20:34:24 +0100

    Adicionar segunda parte do episodio eclipse

--- episodios/universo/eclipse/long/main.py
+++ episodios/universo/eclipse/long/main.py
@@ -6,19 +6,25 @@ from episodios.universo.eclipse.long.cenas.cena_01_intro import (
     executar_cena_01,
 )
 
+from episodios.universo.eclipse.long.cenas.cena_02_segredo_400 import (
+    executar_cena_02,
+)
 
-VERSAO = "EP001_ECLIPSE_LONG_MODULAR_V1"
+
+VERSAO = "EP001_ECLIPSE_LONG_MODULAR_V2"
 
 
 class Episodio001EclipseLong(CenaLong):
     """
     Episódio Long modular sobre eclipses solares.
-
-    Este ficheiro apenas organiza e executa as cenas.
-    Os desenhos e animações ficam nos respetivos módulos.
     """
 
     def construct(self) -> None:
-        print(f"✅ A renderizar {VERSAO}")
+
+        print(
+            f"✅ A renderizar {VERSAO}"
+        )
 
         executar_cena_01(self)
+
+        executar_cena_02(self)
